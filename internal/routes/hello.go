43fccb1c58f438f45a72758a03f9a59285de3196
Cliff Snyder <cliff.snyder@gmail.com>
2025-08-04 11:50:03 -0400

    hello refactor

--- internal/routes/hello.go
+++ internal/routes/hello.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 )
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	name := "World"
-	if r.URL.Query().Has("name") {
-		name = r.URL.Query().Get("name")
+const defaultName string = "World"
+
+func hello(name string) string {
+	if name == "" {
+		name = defaultName
 	}
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	message := hello(r.URL.Query().Get("name"))
 
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
+	w.Write([]byte(message))
 }
