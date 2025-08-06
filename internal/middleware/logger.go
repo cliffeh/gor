c038b6ba1c57e76f2d6f72713b63f6892024e46e
Cliff Snyder <cliff.snyder@gmail.com>
2025-08-06 11:43:46 -0400

    slogging

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,10 +1,22 @@
 package middleware
 
 import (
-	"log"
+	"context"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
 )
+
+const LevelAccess = slog.LevelError * 2
+
+var LevelNames = map[slog.Leveler]string{
+	LevelAccess: "ACCESS",
+}
+
+// func (l *slog.Logger) Access(msg string, args ...any) {
+// 	slog.Log(context.Background(), LevelAccess, msg, args...)
+// }
 
 type loggingResponseWriter struct {
 	http.ResponseWriter
@@ -31,20 +43,41 @@ func (lrw *loggingResponseWriter) WriteHeader(code int) {
 }
 
 func Logger(next http.Handler) http.Handler {
+	opts := slog.HandlerOptions{
+		Level: LevelAccess,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			switch a.Key {
+			case slog.LevelKey:
+				level := a.Value.Any().(slog.Level)
+				levelLabel, exists := LevelNames[level]
+				if !exists {
+					levelLabel = level.String()
+				}
+
+				a.Value = slog.StringValue(levelLabel)
+			case "msg":
+				// Remove the message key from the log output
+				return slog.Attr{}
+			}
+
+			return a
+		},
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &opts))
+	ctx := context.Background()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		lrw := &loggingResponseWriter{ResponseWriter: w}
 		next.ServeHTTP(lrw, r) // Serve the actual request
 		duration := time.Since(start)
 
-		log.Printf("[%s] %s %s %s %d %d %s\n",
-			r.RemoteAddr,
-			r.Method,
-			r.URL.Path,
-			r.Proto,
-			lrw.statusCode,
-			lrw.bytesWritten,
-			duration,
-		)
+		logger.Log(ctx, LevelAccess, "", // empty message
+			"method", r.Method,
+			"path", r.URL.Path,
+			"addr", r.RemoteAddr,
+			"proto", r.Proto,
+			"status", lrw.statusCode,
+			"size", lrw.bytesWritten,
+			"duration", duration)
 	})
 }
